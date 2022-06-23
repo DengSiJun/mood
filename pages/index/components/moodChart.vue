ffc3d9a43213f07fa8f643a144bfff0e66b1f5c9
shingo <shingo@shingodeMacBook-Air.local>
2022-06-24 01:36:54 +0800

    init project

---
+++ pages/index/components/moodChart.vue
@@ -0,0 +1,65 @@
+<template>
+	<view class="container">
+		<view class="chart">
+			<mood-item v-for="item in data" :item="item" :key="item.index" :active="item.index === activeIndex"
+				@click.native="onItemClick(item.index)"></mood-item>
+		</view>
+		<view class="line top"></view>
+		<view class="line middle"></view>
+	</view>
+
+</template>
+
+<script>
+	import moodItem from './moodItem.vue'
+
+	export default {
+		components: {
+			moodItem
+		},
+		props: ['data'],
+		data() {
+			return {
+				activeIndex: -1
+			}
+		},
+		methods: {
+			onItemClick(index) {
+				this.activeIndex = this.activeIndex === index ? -1 : index
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	.container {
+		position: relative;
+		width: 100%;
+	}
+
+	.chart {
+		height: 630rpx;
+		display: flex;
+		justify-content: space-between;
+	}
+
+	.line {
+		background: #F2F2F2;
+		height: 4rpx;
+		z-index: -1;
+	}
+
+	.top {
+		position: absolute;
+		top: 0;
+		left: 0;
+		right: 0;
+	}
+
+	.middle {
+		position: absolute;
+		top: 250rpx;
+		left: 0;
+		right: 0;
+	}
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,67 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/heart.jpeg" mode="aspectFill"></image>
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view>
+		<view class="mood">
+			<button @click="onMoodClick">历史心情指数</button>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			onMoodClick() {
+				uni.navigateTo({
+					url: './mood'
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+	
+	.mood {
+		margin-top: 100rpx;
+	}
+	
+	.mood button {
+		font-size: 30rpx;
+	}
+</style>
